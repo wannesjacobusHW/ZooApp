f87410bed86baea73752112cb4fdb05544ada8bd
wannesjacobusHW <wannes.jacobus@student.howest.be>
2019-11-27 15:27:27 +0100

    Add EntranceFee

---
+++ Zoo.Lib/Zoo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Lib
+{
+    class Zoo
+    {
+        public decimal EntranceFee { get; set; } = 50.0M;
+
+        public Zoo(decimal entranceFee)
+        {
+            EntranceFee = entranceFee;
+        }
+    }
+}
